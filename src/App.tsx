65dcc2f6fb387d8ae5eaa1ba81066fbfd6939ffa
samsularifinweb24-lgtm <samsularifinweb24@gmail.com>
2025-09-30 22:10:34 +0600

    uploaded

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/contact";
-import  Skills  from "./components/Skills";
+import { Skills } from "./components/Skills";
+  
+
 
 
 

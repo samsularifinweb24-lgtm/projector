b5844150094043aefb58f407e4606ad7f7deb260
samsularifinweb24-lgtm <samsularifinweb24@gmail.com>
2025-10-01 10:12:21 +0600

    upded

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
-import { Skills } from "./components/Skills";
-  
-
-
-
-
-
-
-
-
+import Contact from "./components/contact";  
+import Skills from "./components/Skills";     
 
 
 export default function App() {
@@ -22,18 +13,13 @@ export default function App() {
     <div className="bg-gradient-to-r from-gray-900 via-black to-gray-950 text-white min-h-screen">
       <Navbar />
       <Hero />
-      <About/>
       <About />
-      <Skills/>
+      <Skills />
       <Projects />
       <Contact />
-      
-     
-    
     </div>
   );
 }
-
 
 
 
